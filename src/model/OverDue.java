15fe110f67c1849107215ab6a54e8a8296bff171
dharmakshetri <dharma.kshetri@gmail.com>
2016-05-31 11:34:05 -0500

    OverDue Class added

---
+++ src/model/OverDue.java
@@ -0,0 +1,16 @@
+package model;
+
+public class OverDue {
+	private int id;
+	private double overDueAmout;
+	public OverDue(){}
+	
+	public double getOverDueAmout() {
+		return overDueAmout;
+	}
+	public void setOverDueAmout(double overDueAmout) {
+		this.overDueAmout = overDueAmout;
+	}
+	
+
+}
